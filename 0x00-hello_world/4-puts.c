0091868ba447232754881ad5b9f92a1f4377d448
Festus1914 <oladiplentyfestus@gmail.com>
2022-12-08 11:02:26 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - main block 
+ * main - Entry point 
  *   
- * return: 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)    
 {  
-   	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
      
 }
