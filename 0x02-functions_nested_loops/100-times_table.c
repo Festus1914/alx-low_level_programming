ca5021b4322dd83973bd7136e2f7a0a850e4c6b6
Festus1914 <oladiplentyfestus@gmail.com>
2022-12-16 05:58:28 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,7 +21,7 @@ void print_times_table(int n)
 	_putchar('.');
 	_putchar(' ');
 	prod = num * mult;
-	if (prod <= 90)
+	if (prod <= 99)
 	_putchar(' ');
 	if (prod <= 9)
 	_putchar(' ');
@@ -30,7 +30,7 @@ void print_times_table(int n)
 	_putchar((prod / 100) + '0');
 	_putchar(((prod / 10)) % 10 + '0');
 	}
-	else if (prod <= 90 && prod >= 10)
+	else if (prod <= 99 && prod >= 10)
 	{
 	_putchar((prod / 10) + '0' );
 	}
