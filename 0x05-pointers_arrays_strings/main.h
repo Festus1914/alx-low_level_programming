ca7ba422acb5e49821c1ca8f6e7b933436ca372b
Festus1914 <oladiplentyfestus@gmail.com>
2022-12-19 00:25:32 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,5 @@ void reset_to_98(int *n);
 	void puts2(char *str);
 	void puts_half(char *str);
 	void print_array(int *a, int n);
-	char *_strepy(char *dest, char *src);
+	char *_strcpy(char *dest, char *src);
 	void rev_string(char *a);
